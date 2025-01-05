9b99524f518908ece4cd8eb93dce1cb33affa484
Alex <alexgllamosas@gmail.com>
2025-01-05 16:12:59 -0300

    .

--- index.js
+++ index.js
@@ -39,19 +39,19 @@ app.post('/login', async (req, res) => {
   const { id, password } = req.body;
 
   try {
-    // 1. Comprobar si el usuario existe en la BD
+    //  Comprueba si el usuario existe en la BD
     const result = await pool.query(`SELECT * FROM users WHERE id_user = ${id}`);
     if (result.rows.length === 0) {
-      return res.status(401).json({ message: 'Usuario no encontrado' });
+      return res.status(401).json({ message: 'Usuario no encontrado 🤔' });
     }
 
     const user = result.rows[0];
     console.log('Consulta de Log-in con el user: ', user);
-    // contraseña
+    // Comprobación de contraseña
     if (password !== user.password) {
       return res.status(401).json({ message: 'Contraseña incorrecta' });
     }
-    // 3. Crear un token JWT (no implementado)
+    //  Crear un token JWT (no implementado)
     const token = jwt.sign({ id: user.id_user}, 'clave_secreta', { expiresIn: '1h' });
     res.status(200).json({ message: 'Inicio de sesión exitoso', token });
     console.log('Inicio de sesión exitoso');
